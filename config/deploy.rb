5b4d9e91baa8b3384140852defd679a3b0d4fcea
deenaros <deenarosc@gmail.com>
2013-11-15 09:04:53 +0530

    update config files

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :use_sudo, true
 server "ruby-test1.cloudapp.net", :web, :app, :db, primary: true
 
 
-set :deploy_to, "/home/#{user}/#{application}"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 ssh_options[:port] = 80
