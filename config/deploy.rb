c76d857874a370ce0247a4d0299a6333c301cd19
deenaros <deenarosc@gmail.com>
2013-11-16 11:24:35 +0530

    db yml file update

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ server "ruby-test1.cloudapp.net", :web, :app, :db, primary: true
 set :deploy_to, "/home/#{user}/apps/#{application}"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-ssh_options[:port] = 80
+ssh_options[:port] = 22
 
 
 namespace :deploy do
